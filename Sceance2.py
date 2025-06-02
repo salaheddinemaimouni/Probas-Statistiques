e871a48be0752f572332823fafe1a19cda9ef127
Salah Eddine Maimouni <160526967+salaheddinemaimouni@users.noreply.github.com>
2025-06-02 10:08:01 +0200

    Update Sceance2.py

--- Sceance2.py
+++ Sceance2.py
@@ -1,8 +1,11 @@
 from pjt5_1 import *
-
+import os
 
 # Importer
-x, z = importer_donnees(r"C:\Users\salah\Desktop\myfiles\PGE\GIE2\RESA\Mini projet 4 RESA/donnees_sim.xlsx", "Train")
+    # Obtenir le chemin absolu du fichier Excel situé dans le même dossier que le script
+chemin_fichier = os.path.join(os.path.dirname(__file__), "donnees_sim.xlsx")
+    # Appel de la fonction avec le chemin dynamique
+x, z = importer_donnees(chemin_fichier, "Train")
 
 # Normaliser
 x_norm = normaliser_donnees(x)
@@ -10,6 +13,16 @@ x_norm = normaliser_donnees(x)
 
 # Calcul de distance
 dist = creer_matrice_distance(x_norm)
+
+# Afficher la matrice de distance
+plt.figure(figsize=(8, 6))
+plt.imshow(dist, cmap='viridis', interpolation='nearest')
+plt.title("Matrice de distance normalisée")
+plt.colorbar(label='Distance')  # Ajoute une barre de couleur à droite indiquant l'échelle des distances
+plt.xlabel("Point i")   # Étiquette de l'axe des abscisses
+plt.ylabel("Point j")   # Étiquette de l'axe des ordonnées
+plt.tight_layout() # Ajuste automatiquement les marges pour une meilleure mise en page
+plt.show()
 
 
 # Calcul variogramme
